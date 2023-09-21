7eaa813d80bf1398c665a95fed6ddec40e77c604
Shreyansh Shrey <105908147+shreyansh-velotio@users.noreply.github.com>
2023-09-21 17:25:44 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './modules/app/app.module';
 
+// Entry point
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
